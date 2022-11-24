09a5c23b15b50c72384eacafbdeaff94c99973a8
deanabii <117475244+deanabii@users.noreply.github.com>
2022-11-24 16:38:51 +0700

    Add files via upload

---
+++ 3_C_71220908.py
@@ -0,0 +1,7 @@
+pesanan = input("Masukkan daftar pesanan:")
+daftar = pesanan.split()
+print("Daftar pesanan:",daftar)
+
+tambah = input("Masukkan pesanan yang ingin ditambahkan:")
+daftar.append(tambah)
+print("Hasil penambahan pada daftar pesanan:",daftar)
